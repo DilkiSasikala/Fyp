f0a983c8e6a040fc164cdc40101b6cae5b531ebe
Dilki Sasikala <ddsasikala27@gmail.com>
2021-04-29 07:47:57 +0530

    Update to UI

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # importing necessary libraries and functions
 import numpy as np
 import pandas as pd
-from flask import Flask, request, jsonify, render_template, make_response
+import os
+from flask import Flask, flash, request, jsonify, render_template, make_response, redirect
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 import pickle
@@ -9,8 +10,20 @@ import io
 from io import StringIO
 import csv
 import uuid
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 app = Flask(__name__) #Initialize the flask App
+app.secret_key = "abc"  
+
+UPLOAD_FOLDER = "/Volumes/Transcend/IIT LEVEL 6/prototype-fraud/Application"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = {'csv'}
+# ALLOWED_EXTENSIONS = set(['csv'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/') # Homepage
 def home():
@@ -27,8 +40,14 @@ def predict():
     # retrieving values from form
     if request.method == 'POST':
       f = request.files['data_file']
-      if not f:
-        return "No file"
+
+      if f.filename == '':
+        flash('No file is detected. Please enter a CSV file!!')
+        return redirect('/')  
+
+      if not allowed_file(f.filename):
+        flash('Only CSV files are accepted!!') 
+        return redirect('/')  
 
     stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
     csv_input = csv.reader(stream)
@@ -39,6 +58,7 @@ def predict():
     stream.seek(0)
     result = stream.read()
 
+    # add file to dataframe
     df = pd.read_csv(StringIO(result))
     # df = pd.read_csv('newcleaned_test.csv')
     id_value = df['click_id']
@@ -49,9 +69,11 @@ def predict():
 
     # load the model from disk
     loaded_model = pickle.load(open('model.pkl', 'rb'))
+    #prediction start
     prediction = loaded_model.predict(column)
 
     # print (prediction)
+    #save csv
     unique_filename = str(uuid.uuid4())
     res = pd.DataFrame(prediction)
     res.index = id_value
